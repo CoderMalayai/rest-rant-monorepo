a2244ff0ef7d1b6598c907b1ced75b96198e4e42
Malayai Wilson <codermalayaiwilson@gmail.com>
2024-06-24 20:33:38 -0700

    Admin

--- backend/controllers/places.js
+++ backend/controllers/places.js
@@ -1,9 +1,13 @@
 const router = require('express').Router()
 const db = require("../models")
 
+
 const { Place, Comment, User } = db
 
 router.post('/', async (req, res) => {
+    if (req.currentUser?.role !== 'admin') {
+        return res.status(403).json({ message: 'You are not allowed to add a place' })
+    }
     if (!req.body.pic) {
         req.body.pic = 'http://placekitten.com/400/400'
     }
@@ -19,6 +23,7 @@ router.post('/', async (req, res) => {
 
 
 router.get('/', async (req, res) => {
+    console.log("req console.log", req)
     const places = await Place.findAll()
     res.json(places)
 })
@@ -46,6 +51,9 @@ router.get('/:placeId', async (req, res) => {
 
 router.put('/:placeId', async (req, res) => {
     let placeId = Number(req.params.placeId)
+    if (req.currentUser?.role !== 'admin') {
+        return res.status(403).json({ message: 'You are not allowed to add a place' })
+    }
     if (isNaN(placeId)) {
         res.status(404).json({ message: `Invalid id "${placeId}"` })
     } else {
@@ -64,6 +72,9 @@ router.put('/:placeId', async (req, res) => {
 
 router.delete('/:placeId', async (req, res) => {
     let placeId = Number(req.params.placeId)
+    if (req.currentUser?.role !== 'admin') {
+        return res.status(403).json({ message: 'You are not allowed to add a place' })
+    }
     if (isNaN(placeId)) {
         res.status(404).json({ message: `Invalid id "${placeId}"` })
     } else {
